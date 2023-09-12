f0803f0a5cf252459597bad7e93804eb0834de5c
Justin Icenhour <creative515.justin@gmail.com>
2023-09-12 13:58:00 -0500

    update deps

--- src/keys.rs
+++ src/keys.rs
@@ -25,24 +25,32 @@ impl Keys {
 	pub async fn tls_cert_path(&mut self) -> String {
 		let azure_credentials = ImdsManagedIdentityCredential::default();
 		let azure_key_vault_client = KeyvaultClient::new("https://eggappserverkeyvault.vault.azure.net", Arc::new(azure_credentials)).unwrap().secret_client();
-		azure_key_vault_client.get("tls-cert-path").await.unwrap().value
+		let cert_path = azure_key_vault_client.get("tls-cert-path").await.unwrap().value;
+                println!("tls_cert_path: {}", cert_path);
+                cert_path
 	}
 
 	pub async fn tls_cert_pem_path(&mut self) -> String {
 		let azure_credentials = ImdsManagedIdentityCredential::default();
 		let azure_key_vault_client = KeyvaultClient::new("https://eggappserverkeyvault.vault.azure.net", Arc::new(azure_credentials)).unwrap().secret_client();
-		azure_key_vault_client.get("tls-cert-pem-path").await.unwrap().value
+		let cert_path = azure_key_vault_client.get("tls-cert-pem-path").await.unwrap().value;
+                println!("tls-cert-pem-path: {}", cert_path);
+                cert_path
 	}
 
 	pub async fn tls_key_pem_path(&mut self) -> String {
 		let azure_credentials = ImdsManagedIdentityCredential::default();
 		let azure_key_vault_client = KeyvaultClient::new("https://eggappserverkeyvault.vault.azure.net", Arc::new(azure_credentials)).unwrap().secret_client();
-		azure_key_vault_client.get("tls-key-pem-path").await.unwrap().value
+		let cert_path = azure_key_vault_client.get("tls-key-pem-path").await.unwrap().value;
+                println!("tls-key-pem-path: {}", cert_path);
+                cert_path
 	}
 
 	pub async fn tls_rsa_key_pem_path(&mut self) -> String {
 		let azure_credentials = ImdsManagedIdentityCredential::default();
 		let azure_key_vault_client = KeyvaultClient::new("https://eggappserverkeyvault.vault.azure.net", Arc::new(azure_credentials)).unwrap().secret_client();
-		azure_key_vault_client.get("tls-rsa-key-pem-path").await.unwrap().value
+		let cert_path = azure_key_vault_client.get("tls-rsa-key-pem-path").await.unwrap().value;
+                println!("tls-rsa-key-pem-path: {}", cert_path);
+                cert_path
 	}
 }
